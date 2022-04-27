603834c067a0f16972a9ee3ffb406932e6bd7425
joshraiborde <joshraiborde@gmail.com>
2022-04-27 11:53:14 -0400

    added app.get to get all the articles

--- app.js
+++ app.js
@@ -3,37 +3,44 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const mongoose = require('mongoose');
-require("dotenv").config() 
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 const port = 3000;
 var now = new Date();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 app.use(express.static("public"));
 
 // create a new database called wikiDB
-mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true });
 
 // create a new schema called articleSchema
 const articleSchema = {
-    title: String,
-    content: String
-  };
-  
-  // create a new mongoose model called Article, based on the articleSchema
-  // collection name is "Article"
-  // specify the schema, articleSchema
-  const Article = mongoose.model("Article", articleSchema);
+  title: String,
+  content: String
+};
 
-//TODO
+// create a new mongoose model called Article, based on the articleSchema
+// collection name is "Article"
+// specify the schema, articleSchema
+const Article = mongoose.model("Article", articleSchema);
+
+// GET
+app.get("/articles", (req, res) => {
+  // READ
+  Article.find((err, foundArticles) => {
+    res.send(foundArticles);
+  });
+});
 
 app.listen(port, () => {
-    console.log("Server is running on Port " + port + " on " + now.toUTCString());
-  });
-  
+  console.log("Server is running on Port " + port + " on " + now.toUTCString());
+});
